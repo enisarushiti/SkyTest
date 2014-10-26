521e5c3ed9bab9736d2d64e9b48924fabc373541
Jon E Rankin <jr0436@bris.ac.uk>
2014-10-26 15:07:07 +0000

    titles centered

--- jonnytvguide/jonnytvguide/fillChannels.js
+++ jonnytvguide/jonnytvguide/fillChannels.js
@@ -1,26 +1,17 @@
-function returnIntTime(time){
-    var intTime;
-    if(time.slice(-2) == "pm" && time.substring(0,2) != "12"){
-        intTime = parseFloat(time.slice(0, -5)) + (parseFloat(time.substring(0, time.length -2).slice(-2))/60) + 12;
-    }else{
-        intTime = parseFloat(time.slice(0, -5)) + (parseFloat(time.substring(0, time.length -2).slice(-2))/60);
-    }
-    console.log("bit = " + parseFloat(time.substring(0, startTime.length -5 )));
-    return intTime;
-}
-
 
 xmlhttp=new XMLHttpRequest();
 xmlhttp.open("GET","bond.xml",false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseXML; 
+console.log(xmlDoc);
 var numberOfChannels = 6;
 var x=xmlDoc.getElementsByTagName("movie");
 var channels = ["sean_channel", "roger_channel", "roger_channel", "george_channel", "timothy_channel", "pierce_channel", "daniel_channel"];
 var channelHasFilm = {};
+console.log(x);
 for (i=0;i<x.length;i++)
   { 
-    var channel = x[i].childNodes[0].nextSibling.nodeName;  
+    var channel = x[i].firstElementChild.localName;  
     channelHasFilm[channel] = "1";
     var startTime = x[i].childNodes[0].nextSibling.getElementsByTagName("start_time")[0].childNodes[0].nodeValue;
     var endTime = x[i].childNodes[0].nextSibling.getElementsByTagName("end_time")[0].childNodes[0].nodeValue;
@@ -29,6 +20,11 @@ for (i=0;i<x.length;i++)
     var endTimeInt = returnIntTime(endTime);
     var width = ((endTimeInt - startTimeInt)/15)*100;
     var margLeft = ((1/30)*100)+(1/15)*(startTimeInt-9)*100;
+    console.log(startTime);
+    console.log(endTime);
+    console.log(filmName);
+    console.log(margLeft);
+
     $('#'+channel).append("<div id = \"" + filmName + "\" class = \"film box\" style=\"width:" + width + "%; margin-left:" + margLeft + "%;\">"+ filmName + "</div>");
   }
 
@@ -41,6 +37,20 @@ for(i=0; i<channels.length; i++){
 
 
 
+
+
+function returnIntTime(time){
+    var intTime;
+    if(time.slice(-2) == "pm" && time.substring(0,2) != "12"){
+        intTime = parseFloat(time.slice(0, -5)) + (parseFloat(time.substring(0, time.length -2).slice(-2))/60) + 12;
+    }else{
+        intTime = parseFloat(time.slice(0, -5)) + (parseFloat(time.substring(0, time.length -2).slice(-2))/60);
+    }
+   // console.log("bit = " + parseFloat(time.substring(0, startTime.length -5 )));
+    return intTime;
+}
+
+
 function horizontalNavigation(position, event, time) {
     $('html, body').animate({scrollLeft: position}, time);
     event.preventDefault();
@@ -49,25 +59,28 @@ function horizontalNavigation(position, event, time) {
 
 $('.scroll.left').click(function (event) {
     var screenWidth = $( window ).width();
-    var moveAmmount = ((screenWidth -((screenWidth/100)*18))/3.5);
+    var moveAmmount = ((screenWidth -((screenWidth/100)*18-2))/7);
     var rightMov = $("body").scrollLeft() -moveAmmount;
     horizontalNavigation(rightMov, event, 400);
 });
 
 $('.scroll.right').click(function (event) {
     var screenWidth = $( window ).width();
-    var moveAmmount = ((screenWidth -((screenWidth/100)*18))/3.5);
+    var moveAmmount = ((screenWidth -((screenWidth/100)*18-2))/7);
     var leftMov = $("body").scrollLeft() + moveAmmount;
     horizontalNavigation(leftMov, event, 400);
 });
 
 
 $('.film:not(.empty)').click(function (event) {
+        console.log("film clicked on");
        $('#horizon').css('visibility','visible');
        $('#popUpBox').css('display','block');
        $('#popUpText').empty();
        $('#popUpText').append("<p style=\"color:black\">"+"The James Bond Movie "+this.id+" is set to be recorded</p>");
 });
+
+
 
 $('#close').click(function (event) {
        var currentPos = $("body").scrollLeft();
@@ -76,9 +89,26 @@ $('#close').click(function (event) {
        horizontalNavigation(currentPos, event, 0);
 });
 
+
+//Turn the side scroll on/off when hovering on/off the area at the sides of the guide
 $('.scroll').hover(function() {  
+    if($('#popUpBox').css('display') == 'none') {
   $( this ).toggleClass( "active" ).next().stop( true, true ).slideToggle();
+}
 });
 
 
+ $('#channel').click(function SelectName() {
+        var popup = window.open("addNewFilms.html", "Popup", "width=500,height=600");
+        popup.focus();
+        window.somefunction = function(g){
+        fillGuide();
+}
+});
+
+
+
+$( document ).ready(function() {
+
+});
 
--- jonnytvguide/jonnytvguide/getForm.js
+++ jonnytvguide/jonnytvguide/getForm.js
@@ -1,8 +1,101 @@
+
+
 function getFormXml(){
     var sXml;
     var sType;
     var sValue;
-    sXml = "<xml>" + "\n"
+    var channelName;
+    var filmEl;
+    var movieEl =xmlDoc.createElement("movie");
+    var nameEl =xmlDoc.createElement("name");
+    var startEl =xmlDoc.createElement("start_time");
+    var endEl =xmlDoc.createElement("end_time");
+    var nameText, startText, endText;
+    sXml = "<movie>" + "\n"
+    $( "form input" ).each(function() {
+        sType = $(this).attr("name");
+        sValue = $(this).val();
+        if(sType == "actor") {
+            console.log($(this).prop("checked"));
+            if($(this).prop("checked") == true){
+               sXml = sXml + "<" +sValue+">\n"; 
+               channelName = sValue;
+               filmEl=xmlDoc.createElement(sValue);
+            }
+        } else if(sType == "name"){
+            sValue = $(this).val();
+            sXml = sXml + "<" + sType + ">" + sValue + "</" + sType + ">\n";
+            nameText =xmlDoc.createTextNode(sValue);
+            nameEl.appendChild(nameText);
+        } 
+        else if(sType == "start_time"){
+            sValue = $(this).val();
+            sXml = sXml + "<" + sType + ">" + sValue + "</" + sType + ">\n";
+            startText =xmlDoc.createTextNode(sValue);
+            startEl.appendChild(startText);
+        }   
+        else if(sType == "end_time"){
+            sValue = $(this).val();
+            sXml = sXml + "<" + sType + ">" + sValue + "</" + sType + ">\n";
+            endText =xmlDoc.createTextNode(sValue);
+            endEl.appendChild(endText);
+        }     
+    });
+    sXml = sXml + "</" + channelName + ">\n";
+    sXml = sXml + "</movie>" + "\n";
+    x=xmlDoc.getElementsByTagName('movie_data');
+    movieEl.appendChild(filmEl);
+    filmEl.appendChild(nameEl);
+    filmEl.appendChild(startEl);
+    filmEl.appendChild(endEl);
+    x[0].appendChild(movieEl);
+    drawTable(xmlDoc);
+}
+
+$( document ).ready(function() {
+    xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","bond.xml",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML; 
+    drawTable(xmlDoc);
+});
+
+function drawTable(xmlDoc){
+    $("#filmTable").empty();
+    $("#filmTable").append("<table id='table' border='1'></table>");
+    var x=xmlDoc.getElementsByTagName("movie");
+        console.log(xmlDoc);
+        console.log( x);
+    for (i=0;i<x.length;i++)
+      { 
+
+          $("#table").append("<tr><td>" + x[i].firstElementChild.localName + 
+            "</td><td>" + x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + 
+            "</td><td>" + x[i].getElementsByTagName("start_time")[0].childNodes[0].nodeValue+"</td><td>"
+                + x[i].getElementsByTagName("end_time")[0].childNodes[0].nodeValue+"</td></tr>");
+      }
+
+}
+
+function sendDoc() {
+        if (window.opener != null && !window.opener.closed) {
+            window.close();
+            console.log("1");
+        }
+        window.close();
+    }
+
+window.onunload = function (e) {
+    opener.somefunction(xmlDoc); //or
+    opener.document.getElementById('someid').innerHTML = 'update content of parent window';
+};
+
+
+function getFormXmlOld(){
+    var sXml;
+    var sType;
+    var sValue;
+    sXml = "<movie>" + "\n"
     $( "form input" ).each(function() {
         sType = $(this).attr("type");
         sValue = $(this).val();
@@ -13,6 +106,6 @@ function getFormXml(){
             sXml = sXml + "<input type='" + $(this).attr("type") + "'>" + sValue + "</input>" + "\n";
         }   
     });
-    sXml = sXml + "</xml>" + "\n";
+    sXml = sXml + "</movie>" + "\n";
     alert(sXml);
 }
